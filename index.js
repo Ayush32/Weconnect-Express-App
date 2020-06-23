5ad947460732539d153b4679ee9dc804bc62458f
Ayush Gupta <721ayush@gmail.com>
2020-06-23 12:58:42 +0530

    install express-ejs-layout

--- index.js
+++ index.js
@@ -5,13 +5,18 @@
 const express = require('express')
 const app = express();
 const port = 8000;
+// set layout 
+const expressLayouts = require('express-ejs-layouts');
+
+// 
+app.use(expressLayouts);;
 
 // use express router
 
 app.use('/', require('./routes'));
 
 app.set('view engine','ejs');
-app.set('views','./views')
+app.set('views','./views');
 
 
 app.listen(port,function(err){
