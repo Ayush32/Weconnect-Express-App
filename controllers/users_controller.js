b48959302e0f33c92f8e93df8d4e3e863c5bcc4f
Ayush Gupta <721ayush@gmail.com>
2020-06-23 20:53:12 +0530

    setting up User schema

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,3 +7,19 @@ module.exports.profile = function (req, res) {
     titleName: "Profile",
   })
 };
+
+// sign Up
+
+module.exports.signUp = function(req,res){
+  return res.render('user_sign_up', {
+    titleName: "WeConnect | Sign Up",
+  });
+}
+
+// Sign In
+
+module.exports.signIn = function(req,res){
+  return res.render('user_sign_in',{
+    titleName: "WeConnect | Sign In",
+  })
+}
--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,11 @@
 
  const userController = require('../controllers/users_controller');
 
+
+
  router.get('/profile',userController.profile);
+
+ router.get('/sign-up', userController.signUp);
+ router.get('/sign-in', userController.signIn)
 
 module.exports = router;
