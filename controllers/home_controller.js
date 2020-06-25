8e7180d32ba171089dceb52dd0464de9ddc90a64
Ayush Gupta <721ayush@gmail.com>
2020-06-25 19:53:56 +0530

    styling with scss

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -8,6 +8,6 @@ module.exports.home = function(req,res){
     console.log(req.cookies);
     res.cookie('user_id',25)
    return res.render('home',{
-       titleName: "Home",
+       titleName: "Home Page",
    })
 }
--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const sassMiddleware = require('node-sass-middleware')
 
 
 app.use(sassMiddleware({
-    src: '/assets/scss',
-    dest: '/assets//css',
+    src: './assets/scss',
+    dest: './assets/css',
     debug: true,
     outputStyle: 'extended',
     prefix: '/css',
