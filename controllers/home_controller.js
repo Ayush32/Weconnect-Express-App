79fdd584d173a4fc6aa4c89bfb56b9ec0d37f932
Ayush Gupta <721ayush@gmail.com>
2020-06-27 19:01:38 +0530

    display post and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,18 +3,30 @@
  *   All rights reserved.
  */
 
- const Post = require('../models/post')
+ const Post = require('../models/post');
+const { populate } = require('../models/post');
 
 //  Another controller which access by routes
 module.exports.home = function(req,res){
     // console.log(req.cookies);
     // res.cookie('user_id',25)
 
-    Post.find({},function(err,posts){
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         titleName: 'WeConnect | Home',
+    //         posts: posts
+
+    //     });
+    // });
+
+
+    // populate the suer of each post
+    Post.find({}).populate('user').exec(function(err, posts){
         return res.render('home',{
             titleName: 'WeConnect | Home',
             posts: posts
 
-        });
     });
+})
+
 }
