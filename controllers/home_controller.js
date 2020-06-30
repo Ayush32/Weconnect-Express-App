e9494123e93624b016864202c21975be3adb9bc1
Ayush Gupta <721ayush@gmail.com>
2020-06-30 12:17:03 +0530

    converting to async await and error handling

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -6,9 +6,10 @@
 const Post = require('../models/post');
 const { populate } = require('../models/post');
 const User = require('../models/user');
+const { post } = require('../routes/users');
 
 //  Another controller which access by routes
-module.exports.home = function(req,res){
+module.exports.home = async function(req,res){
     // console.log(req.cookies);
     // res.cookie('user_id',25)
 
@@ -20,24 +21,50 @@ module.exports.home = function(req,res){
     //     });
     // });
 
+    try{
+         // populate the suer of each post
+         let posts = await Post.find({})
+           .populate("user")
+           .populate({
+             path: "comments",
+             populate: {
+               path: "user",
+             },
+           });
 
-    // populate the suer of each post
-    Post.find({}).populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
+              let users = await User.find({});
+              return res.render("home", {
+                titleName: "WeConnect | Home",
+                posts: posts,
+                all_users: users,
+              });
+       } 
+       catch(err){
 
-        User.find({}, function(err, users){
-        return res.render('home',{
-            titleName: 'WeConnect | Home',
-            posts: posts,
-            all_users: users
-        });
-    });
-})
+       console.log('Error',err);
+       return;
 
+    }
+
+
+
+   
+
+     
+       
 }
+
+
+// using then
+// Post.find({}).populate('comments').then(function());
+
+// let posts = Post.find({}).populate('comments').exec();
+// posts.then();
+
+// without async and awaited
+// let users =  User.find({}, function(err, users){
+//         return res.render('home',{
+//             titleName: 'WeConnect | Home',
+//             posts: posts,
+//             all_users: users
+//         });
