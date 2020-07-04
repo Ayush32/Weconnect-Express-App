de028abe5431d6454988ac1aeb750a14341eb03d
Ayush Gupta <721ayush@gmail.com>
2020-07-04 13:56:02 +0530

    rendering an API

--- controllers/api/v1/post_api.js
+++ controllers/api/v1/post_api.js
@@ -2,9 +2,49 @@
  *   Copyright (c) 2020 
  *   All rights reserved.
  */
-module.exports.index = function(req, res){
+
+const Post = require('../../../models/post');
+const Comment  = require('../../../models/comment');
+module.exports.index = async function(req, res){
+        let posts = await Post.find({})
+         .sort('-createdAt')
+           .populate("user")
+           .populate({
+             path: "comments",
+             populate: {
+               path: "user",
+             },
+           });
+
     return res.json(200, {
         message: "Lists of posts",
-        posts: []
+        posts: posts
+
     })
 }
+
+module.exports.destroy = async function (req, res) {
+  try {
+    let post = await Post.findById(req.params.id);
+    // if (post.user == req.user.id) {
+      post.remove();
+
+      await Comment.deleteMany({ post: req.params.id });
+
+    
+  
+      return res.json(200, {
+          message: "Post and associated comment deleted Successfully!"
+      })
+    // } else {
+    //   req.flash("error", "You cannot delete this post");
+    //   return res.redirect("back");
+    // }
+  } catch (err) {
+      console.log('******', err)
+    
+    return res.json(500,{
+        message: "Internal Server Error"
+    });
+  }
+};
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -8,4 +8,6 @@ const postApi = require("../../../controllers/api/v1/post_api");
 
 router.get('/', postApi.index);
 
+router.delete('/:id', postApi.destroy);
+
 module.exports = router;
