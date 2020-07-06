32b820d38476d61a3b0b64e231e68097891ed1d1
Ayush Gupta <721ayush@gmail.com>
2020-07-06 11:10:22 +0530

    setting nodemail in config

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,3 +2,40 @@
  *   Copyright (c) 2020 
  *   All rights reserved.
  */
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path')
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: '721ayush@gmail.com',
+        password: 'Ayush@5100'
+    }
+});
+
+let renderTemplate = (data, relativePath) => {
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname,'../views/mailers', relativePath),
+        data,
+        function(err, template){
+            if(err){
+                console.log('error in rendering template');
+                return;
+            }
+            mailHTML = template;
+        }
+
+    )
+
+    return mailHTML;
+}
+
+module.exports =  {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+}
