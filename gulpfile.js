c5765f69a155c27be3a1578e6ffd08465e9e0326
Ayush Gupta <721ayush@gmail.com>
2020-07-24 00:56:00 +0530

    compeleting gulp:: js and images manification

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')
 const cssnano = require('gulp-cssnano')
-const rev = require('gulp-rev')
+const rev = require('gulp-rev');
+const uglify = require('gulp-uglify-es').default;
+const imagemin = require('gulp-imagemin')
+const del = require('del')
 
-
-gulp.task('css', function(){
+// minify  css using gulp
+gulp.task('css', function(done){
     console.log('minifying css.....');
     gulp.src('./assets/sass/**/*.scss')
     .pipe(sass())
@@ -16,7 +19,7 @@ gulp.task('css', function(){
     .pipe(gulp.dest('./assets.css'));
 
 
-    return gulp.src('./assets/**/*.css')
+    gulp.src('./assets/**/*.css')
     .pipe(rev())
     .pipe(gulp.dest('./public/assets'))
     .pipe(rev.manifest({
@@ -24,4 +27,49 @@ gulp.task('css', function(){
         merge: true
     }))
     .pipe(gulp.dest('./public/assets'))
+    done();
 })
+
+// minify js using gulp
+gulp.task('js', function(done){
+    console.log('minifying js......')
+    gulp.src('./assets/**/*.js')
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done()
+  })
+
+//   minify images using gulp
+gulp.task('images', function(done){
+      console.log('minifying images......');
+      gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
+      .pipe(imagemin())
+      .pipe(rev())
+      .pipe(gulp.dest('./public/assets'))
+      .pipe(rev.manifest({
+          cwd: 'public',
+          merge: true
+      }))
+      .pipe(gulp.dest('./public/assets'));
+      done();
+});
+// empty the public / assets directory
+gulp.task('clean:assets', function(done){
+    del.sync('./public/assets');
+    done();
+})
+
+gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'), function(done){
+    console.log('Building assets..');
+    done();
+})
+
+
+
+
