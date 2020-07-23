ebc00859eb5bef47903f6620aa33f107121f1ea8
Ayush Gupta <721ayush@gmail.com>
2020-07-23 11:44:07 +0530

    styling complete

---
+++ assets/js/animation.js
@@ -0,0 +1,28 @@
+/*
+ *   Copyright (c) 2020 
+ *   All rights reserved.
+ */
+var textWrapper = document.querySelector(".ml2");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".ml2 .letter",
+    scale: [4, 1],
+    opacity: [0, 1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70 * i,
+  })
+  .add({
+    targets: ".ml2",
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1500,
+  });
